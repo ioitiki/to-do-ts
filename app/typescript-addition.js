7c211362cb4b6aaec363c865eb567e4fc6a0bb38
Andy Malkin and Nathaniel Meyer <andy@>
2017-05-01 09:18:32 -0700

    create to-do typescript web app using new constructor format

---
+++ app/typescript-addition.js
@@ -0,0 +1,7 @@
+var findSum = function (firstValue, secondValue) {
+    var sum = firstNumber + secondNumber;
+    alert("The sum of your two numbers is: " + sum);
+};
+var firstNumber = parseInt(prompt('please enter a number.'));
+var secondNumber = parseInt(prompt('enter another number.'));
+findSum(firstNumber);
